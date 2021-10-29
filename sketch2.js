e8797914a05a1d5a447ea88bf295fdeda4200dd8
Greg D. Chan <5040157+gregdchan@users.noreply.github.com>
2021-10-28 23:15:15 -0400

    Add files via upload

--- sketch2.js
+++ sketch2.js
@@ -1,8 +1,4 @@
-// ml5.js: Pose Estimation with PoseNet
-// The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/Courses/ml5-beginners-guide/7.1-posenet.html
-// https://youtu.be/OIo-DIOkNVg
-// https://editor.p5js.org/codingtrain/sketches/ULA97pJXR
+
 
 let video;
 let poseNet;
