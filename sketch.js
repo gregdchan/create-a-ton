23f17e71208c13a4d474a8d2b2e2b86b5492bdb0
Greg D. Chan <5040157+gregdchan@users.noreply.github.com>
2021-10-29 01:10:03 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,57 @@
-let vid;
+//camera setup 
+let cam;
 let isVideoLoaded = false;
 let isVideoPlaying = false;
 
-let cam;
-
+//framestrip array setup
 let pastFrames = [];
-let numFrames = 100;
+let numFrames = 150;
 let stripHeight;
 let isRecording = false;
 
-//posenet variables
+//posenet setup
 let poseNet;
 let pose;
 let skeleton;
+
 
 function onVideoLoad() {
   isVideoLoaded = true;
 }
 
 function setup() {
-  createCanvas(windowWidth/1.5, windowHeight/1.5);
+  createCanvas(windowWidth, windowHeight);
   pixelDensity(1);
-
   
-  // vid = createVideo("calvin.mp4", onVideoLoad);
-  // vid.hide();
-
+  //start camera
   cam = createCapture(VIDEO);
   cam.hide();
+  stripHeight = windowHeight / numFrames;
+  poseNet = ml5.poseNet(cam, modelLoaded);
+  poseNet.on('pose', gotPoses);
 
-  stripHeight = height / numFrames;
-
+  //create past frames
   for (let i = 0; i < numFrames; i++) {
     let layer = createGraphics(width, height);
     pastFrames.push(layer);
   }
 }
 
+function gotPoses(poses) {
+    //console.log(poses); 
+    if (poses.length > 0) {
+      pose = poses[0].pose;
+      skeleton = poses[0].skeleton;
+    }
+  }
+  function modelLoaded() {
+    console.log('poseNet ready');
+  }
+
 function draw() {
   background(220);
-
-  // if(isVideoPlaying){
   pastFrames[0].image(cam, 0, 0, width, height);
-  // }
+
 
   for (let i = 0; i < pastFrames.length; i++) {
     image(pastFrames[i], 0, stripHeight * i, width, stripHeight, 0, stripHeight * i, width, stripHeight);
@@ -53,6 +62,32 @@ function draw() {
   }
 
   pastFrames[pastFrames.length - 1] = pastFrames[0];
+
+  if (pose) {
+    let eyeR = pose.rightEye;
+    let eyeL = pose.leftEye;
+    let d = dist(eyeR.x, eyeR.y, eyeL.x, eyeL.y);
+    fill(255, 0, 0);
+    ellipse(pose.nose.x, pose.nose.y, d);
+    fill(0, 0, 255);
+    ellipse(pose.rightWrist.x, pose.rightWrist.y, 32);
+    ellipse(pose.leftWrist.x, pose.leftWrist.y, 32);
+    
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      fill(0,255,0);
+      ellipse(x,y,16,16);
+    }
+    
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255);
+      line(a.position.x, a.position.y,b.position.x,b.position.y);      
+    }
+  }
 
 }
 
